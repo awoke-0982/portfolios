f8264a1be3efc6805819b86b046366cc510fdb48
awoke-0982 <alazereawoke0982@gmail.com>
2024-02-09 23:42:11 -0800

    deploy

---
+++ script.js
@@ -0,0 +1,241 @@
+// active hamburger menu 
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist")
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+// remove navlist
+navlist.addEventListener("click",()=>{
+    navlist.classList.remove("active");
+    menuIcon.classList.remove("active");
+    document.body.classList.remove("open");
+})
+
+
+
+// rotate text js code 
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("");
+
+
+// switch between about buttons 
+
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    contents.forEach(content => content.style.display = 'none');
+    contents[index].style.display = 'block';
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+
+
+// portfolio fillter 
+
+var mixer = mixitup('.portfolio-gallery',{
+    selectors: {
+        target: '.portfolio-box'
+    },
+    animation: {
+        duration: 500
+    }
+});
+
+
+// Initialize swiperjs 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
+    },
+
+    breakpoints: {
+        576:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        1200:{
+            slidesPerView:3,
+            spaceBetween:20,
+        },
+    }
+  });
+
+
+
+//   skill Progress bar 
+
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll",()=>{
+    if(!skillsPlayed)
+    skillsCounter();
+})
+
+
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+    if(window.innerHeight >= topPosition + el.offsetHeight)return true;
+    return false;
+}
+
+function updateCount(num,maxNum){
+    let currentNum = +num.innerText;
+    
+    if(currentNum < maxNum){
+        num.innerText = currentNum + 1;
+        setTimeout(()=>{
+            updateCount(num,maxNum)
+        },12)
+    }
+}
+
+
+let skillsPlayed = false;
+
+function skillsCounter(){
+    if(!hasReached(first_skill))return;
+    skillsPlayed = true;
+    sk_counters.forEach((counter,i)=>{
+        let target = +counter.dataset.target;
+        let strokeValue = 465 - 465 * (target / 100);
+
+        progress_bars[i].style.setProperty("--target",strokeValue);
+
+        setTimeout(()=>{
+            updateCount(counter,target);
+        },400)
+    });
+
+    progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
+}
+
+
+// side progress bar 
+
+let calcScrollValue = ()=>{
+    let scrollProgress = document.getElementById("progress");
+    let pos = document.documentElement.scrollTop;
+
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }else{
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click",()=>{
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#fff ${scrollValue}%,#e6006d ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+// active menu 
+
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+// scroll reveal
+
+ScrollReveal({ 
+    distance:"90px",
+    duration:2000,
+    delay:200,
+    // reset: true ,
+});
+
+
+ScrollReveal().reveal('.hero-info,.main-text,.proposal,.heading', { origin: "top" });
+ScrollReveal().reveal('.about-img,.fillter-buttons,.contact-info', { origin: "left" });
+ScrollReveal().reveal('.about-content,.skills', { origin: "right" });
+ScrollReveal().reveal('.allServices,.portfolio-gallery,.blog-box,footer,.img-hero', { origin: "bottom" });
+
+
+
+
+
+
+
+
+
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("butn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
